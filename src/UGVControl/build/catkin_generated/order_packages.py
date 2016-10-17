97e764e582a0f28bd631299cb5465fb51496a759
AnthonyPichardo <anthony.pichardo.453@my.csun.edu>
2016-10-17 13:49:08 -0700

    Added packges for UGVCOntrol and UGVGui

---
+++ src/UGVControl/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/anthony/UGVControl/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/anthony/Documents/husky_kinetic/devel;/opt/ros/kinetic_src;/opt/ros/kinetic".split(';') if "/home/anthony/Documents/husky_kinetic/devel;/opt/ros/kinetic_src;/opt/ros/kinetic" != "" else []
